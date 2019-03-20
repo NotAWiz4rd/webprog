2f24bd013b86409028ad031f9f47257427d258e5
NotAWiz4rd <xamasa98@gmail.com>
2019-03-20 23:24:33 +0100

    impressum design adjustments

--- src/app/pages/impressum-page/impressum-page.component.ts
+++ src/app/pages/impressum-page/impressum-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Globals} from "../../util/Globals";
+import {LanguageService} from "../../services/language.service";
 
 @Component({
   selector: 'app-impressum-page',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImpressumPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public globals: Globals,
+              public languageService: LanguageService) {
+  }
 
   ngOnInit() {
+    this.globals.view = 'impressum';
   }
 
 }
