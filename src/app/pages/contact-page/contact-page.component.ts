731c5974fa5204574f14cbf4edb4a51723e408a5
robbmue <robinmueller77@gmail.com>
2019-04-14 17:10:35 +0200

    website is not showing up

--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -1,13 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, OnInit} from '@angular/core';
+import {Globals} from '../../util/Globals';
+import {LanguageService} from '../../services/language.service';
+import {FormsModule, NgModel} from "@angular/forms";
+import {BrowserModule} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-contact-page',
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css']
 })
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule
+  ]
+})
+
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  name!: string;
+  email!: string;
+  message!: string;
+
+
+  constructor(public globals: Globals,
+              public languageService: LanguageService) {
+
+  }
+
+  onSend(){
+    console.log("send" )
+    const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
+    alert(allInfo);
+  }
 
   ngOnInit() {
   }
